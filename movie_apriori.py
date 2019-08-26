5fcac6200335bbe280a7d5fde875f414510361ec
朱守亮 <git@git.dxl.cc:node/hunqing.git>
2019-08-26 18:18:34 +0800

    add movie_apriori

--- movie_apriori.py
+++ movie_apriori.py
@@ -1,2 +1,20 @@
 # 分析某导演的电影的 频繁项集和关联规则
 from efficient_apriori import apriori
+import csv 
+director = u'张艺谋'
+file_name = './' + director + '.csv'
+lists = csv.reader(open(file_name, 'r', encoding='utf-8-sig'))
+
+# 数据加载
+data = []
+for names in lists:
+    names_new = []
+    for name in names:
+        # 去掉演员数据中的空格
+        names_new.append(name.strip())
+    data.append(names_new[1:])
+
+# 挖掘频繁项集和关联规则
+itemsets, rules = apriori(data,min_support=0.04, min_confidence=1)
+print('频繁项集', itemsets)
+print('关联规则',rules)
